b1b7bdb606915c2393dc8c0a7242c2e562554210
Jared Slawski <jaredslawski@gmail.com>
2026-03-07 00:03:17 -0700

    Finally a clamped FPS camera jeezus christ

--- Assets/_Scripts/PlayerCharacterController.cs
+++ Assets/_Scripts/PlayerCharacterController.cs
@@ -8,9 +8,6 @@ public class PlayerCharacterController : MonoBehaviour
     private Transform _cameraTransform;
     private Rigidbody _playerRigidbody;    
 
-    private float _currentVerticalSpeed = 0.0f;
-    private float _terminalVelocity = 30.0f;
-
     private PlayerControls _playerControls;
 
     private Vector3 _moveDirection = Vector3.zero;
@@ -18,6 +15,9 @@ public class PlayerCharacterController : MonoBehaviour
     private float _moveAcceleration = 30.0f;
     private float _maxMoveVelocity = 50.0f;
     private float _maxFallVelocity = 30.0f;
+    private float _maxVerticalAngle = 60.0f;
+
+    private Vector3 _lookDelta = Vector3.zero;
 
     private void Awake()
     {
@@ -26,7 +26,10 @@ public class PlayerCharacterController : MonoBehaviour
         this._cameraTransform = Camera.main.transform;
         this._playerRigidbody = GetComponent<Rigidbody>();
 
-        this.SetupPlayerControls();
+        this._playerControls.PlayerMap.Look.performed += this.UpdateLookDirection;
+        this._playerControls.PlayerMap.Look.canceled += this.StopLookDirection;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void OnEnable()
@@ -39,77 +42,15 @@ public class PlayerCharacterController : MonoBehaviour
         this._playerControls.Disable();
     }
 
-    private void SetupPlayerControls()
+    private void UpdateLookDirection(InputAction.CallbackContext context)
     {
-        /*
-        this._playerControls.PlayerMap.Left.performed += this.MoveLeft;        
-        this._playerControls.PlayerMap.Right.performed += this.MoveRight;
-        this._playerControls.PlayerMap.Forward.performed += this.MoveForward;
-        this._playerControls.PlayerMap.Back.performed += this.MoveBack;
-        this._playerControls.PlayerMap.Left.canceled += this.StopDirection;
-        this._playerControls.PlayerMap.Right.canceled += this.StopDirection;
-        this._playerControls.PlayerMap.Forward.canceled += this.StopDirection;
-        this._playerControls.PlayerMap.Back.canceled += this.StopDirection;
-        */
-
+        Vector2 mouseDelta = context.ReadValue<Vector2>();
+        this._lookDelta = new Vector3(-mouseDelta.y, mouseDelta.x, 0.0f);
     }
 
-    private void MoveLeft(InputAction.CallbackContext context)
-    {
-        this._moveDirection = new Vector3(-this._cameraTransform.right.x, this._moveDirection.y, this._moveDirection.z);
-        Debug.LogError("Right: " + this._cameraTransform.right);
-    }
-
-    private void MoveRight(InputAction.CallbackContext context)
-    {
-        this._moveDirection = new Vector3(this._cameraTransform.right.x, this._moveDirection.y, this._moveDirection.z);
-    }
-
-    private void MoveForward(InputAction.CallbackContext context)
-    {
-        this._moveDirection = new Vector3(this._moveDirection.x, this._moveDirection.y, this._cameraTransform.forward.z);
-    }
-    private void MoveBack(InputAction.CallbackContext context)
-    {
-        this._moveDirection = new Vector3(this._moveDirection.x, this._moveDirection.y, -this._cameraTransform.forward.z);
-    }
-
-    private void StopDirection(InputAction.CallbackContext context)
-    {
-        this.StopHorizontalMovement();
-        this.StopForwardBackMovement();
-    }
-
-    private void StopHorizontalMovement()
-    {
-        if (this._playerControls.PlayerMap.Left.inProgress == false && this._playerControls.PlayerMap.Right.inProgress == false)
-        {
-            this._moveDirection = new Vector3(0.0f, this._moveDirection.y, this._moveDirection.z);
-        }
-        else if (this._playerControls.PlayerMap.Left.inProgress == true)
-        {
-            this._moveDirection = new Vector3(-this._cameraTransform.right.x, this._moveDirection.y, this._moveDirection.z);
-        }
-        else if (this._playerControls.PlayerMap.Right.inProgress == true)
-        {
-            this._moveDirection = new Vector3(this._cameraTransform.right.x, this._moveDirection.y, this._moveDirection.z);
-        }
-    }
-
-    private void StopForwardBackMovement()
-    {
-        if (this._playerControls.PlayerMap.Forward.inProgress == false && this._playerControls.PlayerMap.Back.inProgress == false)
-        {
-            this._moveDirection = new Vector3(this._moveDirection.x, this._moveDirection.y, 0.0f);
-        }
-        else if (this._playerControls.PlayerMap.Forward.inProgress == true)
-        {
-            this._moveDirection = new Vector3(this._moveDirection.x, this._moveDirection.y, this._cameraTransform.forward.z);
-        }
-        else if (this._playerControls.PlayerMap.Back.inProgress == true)
-        {
-            this._moveDirection = new Vector3(this._moveDirection.x, this._moveDirection.y, -this._cameraTransform.forward.z);
-        }
+    private void StopLookDirection(InputAction.CallbackContext context)
+    { 
+        this._lookDelta = Vector3.zero;
     }
 
     private void Update()
@@ -121,7 +62,25 @@ public class PlayerCharacterController : MonoBehaviour
     {
         this._playerRigidbody.AddForce(this._moveDirection * this._moveAcceleration, ForceMode.Acceleration);
 
+        this._cameraTransform.transform.Rotate(Vector3.up, this._lookDelta.y, Space.World);
+
+        this._cameraTransform.rotation = Quaternion.Euler(this.GetClampedXAngle(), this._cameraTransform.rotation.eulerAngles.y, 0.0f);
+
         this.CapMaxVelocity();
+    }
+
+    private float GetClampedXAngle()
+    {
+        float xRotation = this._cameraTransform.rotation.eulerAngles.x + this._lookDelta.x;
+
+        if (xRotation > 180.0f)
+        {
+            xRotation -= 360.0f;
+        }
+
+        xRotation = Mathf.Clamp(xRotation, -this._maxVerticalAngle, this._maxVerticalAngle);
+
+        return xRotation;
     }
 
     private void CapMaxVelocity()
@@ -177,25 +136,4 @@ public class PlayerCharacterController : MonoBehaviour
 
         return returnVector.normalized;
     }
-
-    /*
-    private void ApplyGravity()
-    {
-        if (this._characterController.isGrounded)
-        {
-            this._currentVerticalSpeed = 0.0f;
-        }
-
-        float accelerationAmountPerFrame = Physics.gravity.y * Time.deltaTime;
-
-        this._currentVerticalSpeed -= accelerationAmountPerFrame;
-
-        if (this._currentVerticalSpeed > (this._terminalVelocity * Time.deltaTime))
-        {
-            this._currentVerticalSpeed = (this._terminalVelocity * Time.deltaTime);
-        }
-
-        this._characterController.Move(Vector3.down * this._currentVerticalSpeed);
-    }
-    */
 }
